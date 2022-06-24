7c5b09e1effb198f631d2b5f30961714d3a9eddb
crist1114 <cristianmanuelmp@ufps.edu.co>
2022-06-24 07:21:41 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const tasksRoutes = require('./routes/tasks');
 const cors = require('cors')
 
 const app = express();
-app.set('port', 3002);
+app.set('port', 8080);
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -22,23 +22,31 @@ app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 	next();
 });
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-            // Dirección desde donde se pueden hacer peticiones
-            if (!["http://localhost:4200"].includes(origin)) {
-                return callback(new Error(`La política CORS para el origen ${origin} no permiten el acceso al servidor.`), false);
-            }
-            return callback(null, true);
-        }
-    })
-);
+//options para poder eliminar
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+    
+    next();
+});
+// app.use(
+//     cors({
+//         origin: function (origin, callback) {
+//             if (!origin) return callback(null, true);
+//             // Dirección desde donde se pueden hacer peticiones
+//             if (!["http://localhost:4200"].includes(origin)) {
+//                 return callback(new Error(`La política CORS para el origen ${origin} no permiten el acceso al servidor.`), false);
+//             }
+//             return callback(null, true);
+//         }
+//     })
+// );
 app.use(myconnection(mysql, {
   host: 'remotemysql.com',
   user: 'fTjRcw87Fe',
